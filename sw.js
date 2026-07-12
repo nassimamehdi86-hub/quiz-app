cf332e676d95f551a68be10d10485806286e0bb3
nassimamehdi86-hub <nassimamehdi86@gmail.com>
2026-07-12 23:34:06 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,50 @@
-const CACHE_NAME = 'english-vocab-cache-v1';
-const ASSETS = [
+const CACHE_VERSION = 'v3';
+const CACHE_NAME = 'quiz-app-fr-' + CACHE_VERSION;
+
+const FILES_TO_CACHE = [
   './',
   './index.html',
-  './sequence1.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './patrimoine.html',
+  './sequence1.html',
+  './sequence2.html',
+  './violence_racisme_quiz-1.html',
+  './vivre_en_paix_quiz-1.html',
+  './protegeons_la_nature_quiz.html',
+  './agir_eco_responsable_quiz.html'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match('./index.html'));
-    })
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
